bb7abe02b34d7d5e65c70922e80713510a91779b
Vlad <iptokmach@gmail.com>
2021-07-12 20:40:04 +0200

    edits at code review

--- vue.js
+++ vue.js
@@ -1,13 +1,9 @@
-// вы можете как угодно изменять программу и код
-// добавлять любые переменные и модели
-// ваша задача реализовать так, как показано на видео, чтобы оно работало
-
 const App = {
   data() {
     return {
-      activeIndex: 0, // то, что позволяет определить текущий активный шаг
-      next: 'Вперед',
-      finished: false,
+      activeIndex: 0,
+      nextBtnText: 'Вперед',
+      isFinished: false,
       steps: [
         {title: 'Основы', text: 'В блоке вы познакомитесь со всеми основами Vue.js на практике. На протяжении блока мы напишем реактивное приложение, в процессе разработки которого разберем вся базу фреймворка.'},
         {title: 'Компоненты', text: 'Один из самых важных блоков в курсе, где вы узнаете все о компонентах. В блоке мы напишем 2 разных приложения и создадим более 5 различных UI компонентов как в реальной разработке. Блок расскажет про абсолютно все составляющие, которые есть в компонентах: взаимодействие, slots, асинхронные и динамические компоненты и тонна примеров.'},
@@ -19,43 +15,38 @@ const App = {
   },
   methods: {
     prev() {
-      // когда нажимаем кнопку назад
       if (this.activeIndex === 0) return;
       this.activeIndex--;
     },
     reset() {
-      // начать заново
       this.activeIndex = 0;
-      this.finished = false;
+      this.isFinished = false;
     },
     nextOfFinish() {
-      // кнопка вперед или закончить
-      if (this.activeIndex === this.steps.length - 1) {
-        this.finished = true;
+      if (this.isLastStep) {
+        this.isFinished = true;
         return;
       }
       this.activeIndex++;
     },
     setActive(idx) {
-      // когда нажимаем на определенный шаг
       this.activeIndex = idx;
     }
   },
   computed: {
-    // тут стоит определить несколько свойств:
-    // 1. текущий выбранный шаг
-    // 2. выключена ли кнопка назад
-    // 3. находимся ли мы на последнем шаге
     currentStep() {
-      return this.activeIndex;
+      return this.steps[this.activeIndex];
     },
-    prevBtnDisabled() {
+    isDisabled() {
       return !this.activeIndex;
     },
+    isLastStep() {
+      return this.activeIndex === this.steps.length - 1;
+    },
     lastStep() {
-      return this.activeIndex === this.steps.length - 1
-        ? this.next = 'Закончить'
-        : this.next = 'Вперед';
+      return this.isLastStep
+        ? this.nextBtnText = 'Закончить'
+        : this.nextBtnText = 'Вперед';
     }
   }
 }
